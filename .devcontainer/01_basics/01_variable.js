ef81059bb8f492d131e31d1bedbf6a856d540914
Prakhar Panchal <127473637+Prakharpanchal7240@users.noreply.github.com>
2024-12-28 11:33:05 +0000

    update

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,9 @@
+let name = "Prakhar"
+const number = 7240914545
+var city = "Indore"
+
+/*
+var ko use nhi karna hai because of issue in block scop and functional scope
+*/
+let state;
+console.table([name, number, city,state])
---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //teat all js code as newer version
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
